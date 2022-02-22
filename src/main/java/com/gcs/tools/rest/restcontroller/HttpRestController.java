dee6594a4e6f31f4488aaeea29b048ed4883c3c9
kg5643 <gitusergcs@gmail.com>
2022-02-22 16:04:35 -0500

    first working version

---
+++ src/main/java/com/gcs/tools/rest/restcontroller/HttpRestController.java
@@ -0,0 +1,316 @@
+/****************************************************************************
+ * FILE: TxFileParserEntryPoint.java
+ * DSCRPT: 
+ ****************************************************************************/
+
+
+
+
+
+package com.gcs.tools.rest.restcontroller;
+
+
+
+
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.EnumSet;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+
+import javax.servlet.DispatcherType;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ErrorHandler;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import org.eclipse.jetty.util.component.LifeCycle;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.fasterxml.jackson.jaxrs.xml.JacksonXMLProvider;
+import com.fasterxml.jackson.jaxrs.yaml.JacksonJaxbYAMLProvider;
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+
+
+@Slf4j
+public class HttpRestController implements LifeCycle.Listener
+{
+
+    @Getter @Setter private boolean _canRegister;
+    private ReentrantLock           _lock;
+
+
+    private ResourceConfig _resourceConfig;
+    private Server         _server;
+
+
+
+
+
+    public HttpRestController(String appName_, int httpPort_) throws Exception
+    {
+        _server = new Server();
+        ServerConnector connector = new ServerConnector(_server);
+        connector.setPort(httpPort_);
+        _server.setConnectors(new Connector[]
+        {
+                connector
+        });
+
+
+        ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+        ctx.setContextPath("/");
+
+
+        FilterHolder filterHolder = new FilterHolder(new CrossOriginFilter());
+        filterHolder.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        filterHolder.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_CREDENTIALS_HEADER, "true");
+        filterHolder.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_METHODS_HEADER, "POST,OPTIONS,GET");
+        filterHolder.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_HEADERS_HEADER, "Content-Type");
+        filterHolder.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_CREDENTIALS_HEADER, "false");
+        filterHolder.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_CREDENTIALS_HEADER, "false");
+        ctx.addFilter(filterHolder, "/*", EnumSet.of(DispatcherType.REQUEST));
+
+
+        _server.setHandler(ctx);
+        _server.addLifeCycleListener(this);
+
+        _resourceConfig = new ResourceConfig();
+        registerProviders();
+        registerSwagger();
+        setCanRegister(true);
+
+
+
+        String pathSpec = "/" + appName_ + "/*";
+        ServletContainer container = new ServletContainer(_resourceConfig);
+        ServletHolder servletHolder = new ServletHolder(container);
+        servletHolder.setInitParameter("idleTimout", "1");
+        ctx.addServlet(servletHolder, pathSpec);
+
+
+
+        ctx.setErrorHandler(new ErrorHandler()
+        {
+
+
+            @Override
+            public void doError(String target_, org.eclipse.jetty.server.Request baseRequest_, HttpServletRequest request_, HttpServletResponse response_) throws IOException
+            {
+                _logger.error("target:{}", target_);
+                super.doError(target_, baseRequest_, request_, response_);
+            }
+
+
+
+
+
+            @Override
+            protected void handleErrorPage(HttpServletRequest request_, Writer writer_, int code_, String message_) throws IOException
+            {
+                _logger.error("target:{}", request_.getRequestURI());
+                super.handleErrorPage(request_, writer_, code_, message_);
+            }
+
+
+
+
+
+            @Override
+            public boolean isShowStacks()
+            {
+                return true;
+            }
+
+
+
+
+
+            @Override
+            public boolean getShowMessageInTitle()
+            {
+                return true;
+
+            }
+
+        });
+
+    }
+
+
+
+
+
+    private void registerSwagger()
+    {
+    }
+
+
+
+
+
+    private void registerProviders()
+    {
+        if (_resourceConfig == null)
+        {
+        }
+
+        _resourceConfig.register(JacksonXMLProvider.class);
+        _resourceConfig.register(JacksonJaxbJsonProvider.class);
+        _resourceConfig.register(JacksonJaxbYAMLProvider.class);
+    }
+
+
+
+
+
+    public void start() throws HttpRestException
+    {
+        if (_server == null)
+        {
+            throw new HttpRestException("Server not initialized");
+        }
+
+        try
+        {
+            _server.start();
+            System.out.println(_server.dump());
+        }
+        catch (Exception ex_)
+        {
+            _logger.error(ex_.toString(), ex_);
+        }
+    }
+
+
+
+
+
+    public void register(Object o_) throws HttpRestException
+    {
+        if (_server == null)
+        {
+            throw new HttpRestException("server is NULL, cannot register");
+        }
+        else if (!_canRegister)
+        {
+            throw new HttpRestException("no longer accepting registration of new end-points");
+        }
+
+        _resourceConfig.register(o_);
+
+    }
+
+
+
+
+
+    @Override
+    public void lifeCycleStarting(LifeCycle event_)
+    {
+        if (_logger.isInfoEnabled())
+        {
+            _logger.info("HttpRestController::lifeCycleStarting(event_)");
+        }
+    }
+
+
+
+
+
+    @Override
+    public void lifeCycleStarted(LifeCycle event_)
+    {
+        if (_logger.isInfoEnabled())
+        {
+            _logger.info("HttpRestController::lifeCycleStarted(enclosing_method_arguments)");
+        }
+    }
+
+
+
+
+
+    @Override
+    public void lifeCycleFailure(LifeCycle event_, Throwable cause_)
+    {
+        _logger.error("LifeCycleFailure:{}, cause:{}", event_.toString(), cause_.toString());
+        _canRegister = false;
+        _server = null;
+    }
+
+
+
+
+
+    @Override
+    public void lifeCycleStopping(LifeCycle event_)
+    {
+        _logger.info("LifeCycleStopping:{}, cause:{}", event_.toString());
+        _canRegister = false;
+    }
+
+
+
+
+
+    @Override
+    public void lifeCycleStopped(LifeCycle event_)
+    {
+        _logger.info("LifeCycleStopping:{}, cause:{}", event_.toString());
+        _server = null;
+    }
+
+
+
+
+
+    public void join() throws InterruptedException
+    {
+        if (_server == null)
+        {
+            _logger.error("can't join, server=NULL");
+            return;
+        }
+        _server.join();
+    }
+
+
+
+
+
+    public void print()
+    {
+        for (var v : _server.getChildHandlers())
+        {
+            System.out.println(v.toString());
+        }
+    }
+
+
+
+
+
+}
---
+++ src/test/java/com/gcs/tools/rest/restcontroller/RestTest.java
@@ -0,0 +1,157 @@
+/**
+ * Author: kgoldstein
+ * Date: Feb 21, 2022
+ * Terms: Expressly forbidden for use without written consent from the author
+ */
+
+
+
+
+
+package com.gcs.tools.rest.restcontroller;
+
+
+
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXB;
+
+
+
+import com.fasterxml.jackson.jaxrs.yaml.YAMLMediaTypes;
+
+
+
+
+
+@Path("rtest")
+public class RestTest
+{
+
+
+
+
+    public static final MediaType YAML_MEDIA_TYPE = YAMLMediaTypes.APPLICATION_JACKSON_YAML_TYPE;
+
+
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("halt")
+    public Response halt()
+    {
+        Thread r = new Thread()
+        {
+
+            @Override
+            public void run()
+            {
+                try
+                {
+                    Thread.sleep(10000);
+                    System.exit(0);
+                }
+                catch (InterruptedException ex_)
+                {
+                    ex_.printStackTrace();
+                }
+            }
+
+        };
+        r.start();
+
+
+        return Response.status(Response.Status.OK).entity(new String("shutting down system in: 10 sec")).build();
+    }
+
+
+
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("ftest")
+    public Response test()
+    {
+        return Response.status(Response.Status.OK).entity(new String("test")).build();
+
+    }
+
+
+
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("larger")
+    public Response larger()
+    {
+        Rsps rsps = new Rsps();
+        rsps.setA(1);
+        rsps.setB("b");
+        var map = new HashMap<String, String>();
+        map.put("abc", "def");
+        map.put("fed", "zuy");
+        HashSet<Object> o = new HashSet<Object>();
+        o.add(map);
+        o.add(rsps);
+        return Response.status(Response.Status.OK).entity(o).build();
+    }
+
+
+
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("larger2")
+    public Response larger2()
+    {
+        Rsps rsps = new Rsps();
+        rsps.setA(1);
+        rsps.setB("b");
+        var map = new HashMap<String, String>();
+        map.put("abc", "def");
+        map.put("fed", "zuy");
+        HashSet<Object> o = new HashSet<Object>();
+        o.add(map);
+        o.add(rsps);
+        return Response.status(Response.Status.OK).entity(o).build();
+    }
+
+
+
+
+
+    @GET
+    @Produces(YAMLMediaTypes.APPLICATION_JACKSON_YAML)
+    @Path("larger3")
+    public Response largerYaml()
+    {
+        Rsps rsps = new Rsps();
+        rsps.setA(1);
+        rsps.setB("b");
+        var map = new HashMap<String, String>();
+        map.put("abc", "def");
+        map.put("fed", "zuy");
+        HashSet<Object> o = new HashSet<Object>();
+        o.add(map);
+        o.add(rsps);
+        return Response.status(Response.Status.OK).entity(o).build();
+    }
+
+
+
+}
