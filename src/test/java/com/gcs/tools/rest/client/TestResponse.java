4cfdf72bc6535092fe18a798561d56b7197206e4
tania.drab <tania.drab@gmail.com>
2022-06-28 17:33:29 +0200

    RestClient now handles refId passed to it and uses connection pool.

---
+++ src/test/java/com/gcs/tools/rest/client/TestResponse.java
@@ -0,0 +1,19 @@
+package com.gcs.tools.rest.client;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TestResponse
+{
+    @JsonProperty("refId") String _refId;
+    @JsonProperty("bodyAsString") String _bodyAsString;
+}
