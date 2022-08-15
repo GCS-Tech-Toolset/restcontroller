3d34eccf06c7f061736a0d2063a422b499f48a57
Jonathan Frederick <jfrederick@q5id.com>
2022-08-15 13:37:06 -0500

    Fixup many warnings and unneeded imports

--- src/test/java/com/gcs/tools/rest/restcontroller/HttpRestControllerTest.java
+++ src/test/java/com/gcs/tools/rest/restcontroller/HttpRestControllerTest.java
@@ -18,18 +18,13 @@ import static org.junit.Assert.assertTrue;
 
 
 
-import java.util.HashMap;
-import java.util.HashSet;
 
 
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 
--- src/test/java/com/gcs/tools/rest/restcontroller/RestTest.java
+++ src/test/java/com/gcs/tools/rest/restcontroller/RestTest.java
@@ -22,10 +22,8 @@ import java.util.HashSet;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.xml.bind.JAXB;
 
 
 
