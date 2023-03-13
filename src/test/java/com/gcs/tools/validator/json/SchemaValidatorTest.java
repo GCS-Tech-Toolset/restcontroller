0c0fa437a046da83ddf39c092249ec5b44e977d8
kagr <kevin@goldsteinconsultingservices.com>
2023-03-13 13:32:31 -0400

    finishing touches

--- src/test/java/com/gcs/tools/validator/json/SchemaValidatorTest.java
+++ src/test/java/com/gcs/tools/validator/json/SchemaValidatorTest.java
@@ -19,11 +19,6 @@ import static org.junit.Assert.fail;
 
 
 
-import java.io.IOException;
-
-
-
-import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -32,62 +27,71 @@ import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 
 
 
+import lombok.SneakyThrows;
+
+
+
 
 
 public class SchemaValidatorTest
 {
 
-	@Ignore @Test
+	@Test @SneakyThrows
 	public void test()
 	{
-		try
-		{
-			SchemaValidator validator = new SchemaValidator();
-			validator.regiseter(Floor.class, "/exmple-json-schema.json");
-			String json = "{\"name\":\"floor2\",\"id\":\"0ae298c5-2c17-49b9-a8ed-7681dd3e1743\"}";
+		SchemaValidator validator = new SchemaValidator();
+		validator.regiseter(Floor.class, "/exmple-json-schema.json");
+		String json = "{\"name\":\"floor2\",\"id\":\"0ae298c5-2c17-49b9-a8ed-7681dd3e1743\"}";
 
-			Floor f = validator.validate(Floor.class, json);
-			assertNotNull(f);
-			assertEquals("floor2", f.getName());
-			assertEquals("0ae298c5-2c17-49b9-a8ed-7681dd3e1743", f.getId());
-		}
-		catch (ProcessingException ex_)
-		{
-			fail(ex_.toString());
-		}
-		catch (IOException ex_)
-		{
-			fail(ex_.toString());
-		}
+		Floor f = validator.validate(Floor.class, json);
+		assertNotNull(f);
+		assertEquals("floor2", f.getName());
+		assertEquals("0ae298c5-2c17-49b9-a8ed-7681dd3e1743", f.getId());
 	}
 
 
 
 
 
-	@Test
+	@Test(expected = ProcessingException.class) @SneakyThrows
 	public void testMissingJsonObject()
 	{
+		SchemaValidator validator = new SchemaValidator();
+		validator.regiseter(Floor.class, "/exmple-json-schema.json");
+		String json = "{\"name\":\"floor2\"}";
 
-		try
-		{
-			SchemaValidator validator = new SchemaValidator();
-			validator.regiseter(Floor.class, "/exmple-json-schema.json");
-			String json = "{\"name\":\"floor2\"}";
+		validator.validate(Floor.class, json);
+		fail("expected exception:missing object");
+	}
 
-			Floor f = validator.validate(Floor.class, json);
-			assertNotNull(f);
-			assertEquals("floor2", f.getName());
-			assertEquals("0ae298c5-2c17-49b9-a8ed-7681dd3e1743", f.getId());
-		}
-		catch (ProcessingException ex_)
-		{
-			fail(ex_.toString());
-		}
-		catch (IOException ex_)
-		{
-			fail(ex_.toString());
-		}
+
+
+
+
+	@Test(expected = ProcessingException.class) @SneakyThrows
+	public void testBadUuid()
+	{
+		SchemaValidator validator = new SchemaValidator();
+		validator.regiseter(Floor.class, "/exmple-json-schema.json");
+		String json = "{\"name\":\"floor2\",\"id\":\"0ae298c5\"}";
+
+		validator.validate(Floor.class, json);
+		fail("expected exception:uuid bad format");
+	}
+
+
+
+
+
+	@Test(expected = ProcessingException.class) @SneakyThrows
+	public void testBadSized()
+	{
+		SchemaValidator validator = new SchemaValidator();
+		validator.regiseter(Floor.class, "/exmple-json-schema.json");
+		String json = "{\"name\":\"f\",\"id\":\"0ae298c5\"}";
+
+		validator.validate(Floor.class, json);
+		fail("expected exception:bad sized");
 	}
 
 }
